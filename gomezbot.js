7140f7168346e9c6aa1c97278078caef83cbe7fe
jhesid2003 <90399687+jhesid2003@users.noreply.github.com>
2021-09-10 22:38:36 -0400

    Update gomezbot.js

--- gomezbot.js
+++ gomezbot.js
@@ -27,25 +27,25 @@ async function iniciar () {
         fs.writeFileSync('./GomezBot.json', JSON.stringify(client.base64EncodedAuthInfo(), null, '\t'))
         
 
-client.on('chat-update', async (choute) => {
+client.on('chat-update', async (gomez) => {
 try {	  
-if (!choute.hasNewMessage) return
-if (!choute.messages) return
-if (choute.key && choute.key.remoteJid == 'status@broadcast') return
+if (!gomez.hasNewMessage) return
+if (!gomez.messages) return
+if (gomez.key && gomez.key.remoteJid == 'status@broadcast') return
 
-choute = choute.messages.all()[0]
-if (!choute.message) return
+gomez = gomez.messages.all()[0]
+if (!gomez.message) return
 global.blocked
-choute.message = (Object.keys(choute.message)[0] === 'ephemeralMessage') ? choute.message.ephemeralMessage.message : choute.message
-const from = choute.key.remoteJid
-const type = Object.keys(choute.message)[0]        
-const quoted = type == 'extendedTextMessage' && choute.message.extendedTextMessage.contextInfo != null ? choute.message.extendedTextMessage.contextInfo.quotedMessage || [] : []
+gomez.message = (Object.keys(gomez.message)[0] === 'ephemeralMessage') ? gomez.message.ephemeralMessage.message : gomez.message
+const from = gomez.key.remoteJid
+const type = Object.keys(gomez.message)[0]        
+const quoted = type == 'extendedTextMessage' && gomez.message.extendedTextMessage.contextInfo != null ? gomez.message.extendedTextMessage.contextInfo.quotedMessage || [] : []
 const typeQuoted = Object.keys(quoted)[0]
-const content = JSON.stringify(choute.message)
+const content = JSON.stringify(gomez.message)
 const { text, extendedText, contact, location, liveLocation, image, video, sticker, document, audio, product } = MessageType
-const body = choute.message.conversation || choute.message[type].caption || choute.message[type].text || ""
-chats = (type === 'conversation') ? choute.message.conversation : (type === 'extendedTextMessage') ? choute.message.extendedTextMessage.text : ''
-budy = (type === 'conversation' && choute.message.conversation.startsWith(prefix)) ? choute.message.conversation : (type == 'imageMessage') && choute.message.imageMessage.caption.startsWith(prefix) ? choute.message.imageMessage.caption : (type == 'videoMessage') && choute.message.videoMessage.caption.startsWith(prefix) ? choute.message.videoMessage.caption : (type == 'extendedTextMessage') && choute.message.extendedTextMessage.text.startsWith(prefix) ? choute.message.extendedTextMessage.text : ''
+const body = gomez.message.conversation || gomez.message[type].caption || gomez.message[type].text || ""
+chats = (type === 'conversation') ? gomez.message.conversation : (type === 'extendedTextMessage') ? gomez.message.extendedTextMessage.text : ''
+budy = (type === 'conversation' && gomez.message.conversation.startsWith(prefix)) ? gomez.message.conversation : (type == 'imageMessage') && gomez.message.imageMessage.caption.startsWith(prefix) ? gomez.message.imageMessage.caption : (type == 'videoMessage') && gomez.message.videoMessage.caption.startsWith(prefix) ? gomez.message.videoMessage.caption : (type == 'extendedTextMessage') && gomez.message.extendedTextMessage.text.startsWith(prefix) ? gomez.message.extendedTextMessage.text : ''
 
 if (prefix != "") {
 if (!body.startsWith(prefix)) {
@@ -70,11 +70,11 @@ const soyYo = client.user.jid
 const botNumber = client.user.jid.split("@")[0]
 const ownerNumber = ['@s.whatsapp.net']
 const isGroup = from.endsWith('@g.us')
-const sender = choute.key.fromMe ? client.user.jid : isGroup ? choute.participant : choute.key.remoteJid
+const sender = gomez.key.fromMe ? client.user.jid : isGroup ? gomez.participant : gomez.key.remoteJid
 const senderNumber = sender.split("@")[0]
 const isMe = senderNumber == botNumber
-const conts = choute.key.fromMe ? client.user.jid : client.contacts[sender] || { notify: jid.replace(/@.+/, '') }
-const pushname = choute.key.fromMe ? client.user.name : conts.notify || conts.vname || conts.name || '-'
+const conts = gomez.key.fromMe ? client.user.jid : client.contacts[sender] || { notify: jid.replace(/@.+/, '') }
+const pushname = gomez.key.fromMe ? client.user.name : conts.notify || conts.vname || conts.name || '-'
 
 
 if(body.includes('bot')) {
@@ -88,7 +88,7 @@ message: {
 }
         
 if(body == ('hola')) {
-client.sendMessage(from, 'como estas!', MessageType.text, {quoted: choute})
+client.sendMessage(from, 'como estas!', MessageType.text, {quoted: gomez})
 }
 
 
@@ -142,18 +142,18 @@ break
 		
 case 'foto':
 const imagen = fs.readFileSync('./media/foto.jpg')                
-client.sendMessage(from, imagen, MessageType.image, {quoted: choute, caption: `*Foto enviada con exito :D*`})
+client.sendMessage(from, imagen, MessageType.image, {quoted: gomez, caption: `*Foto enviada con exito :D*`})
 break
                 
 case 'video':
 const video = fs.readFileSync('./media/video.mp4')
-client.sendMessage(from, video, MessageType.video, {quoted: choute, mimetype: 'video/mp4', caption: 'JAJAJA', duration: 999999999})
+client.sendMessage(from, video, MessageType.video, {quoted: gomez, mimetype: 'video/mp4', caption: 'JAJAJA', duration: 999999999})
 break
                 
 case 'audio':
 const audio = fs.readFileSync('./media/audio.mp3')
-client.sendMessage(from, audio, MessageType.audio, {quoted: choute, mimetype: 'audio/mp3', duration: -9999999, ptt: true})
-client.sendMessage(from, audio, MessageType.audio, {quoted: choute, mimetype: 'audio/mp3', duration: -9999999})                
+client.sendMessage(from, audio, MessageType.audio, {quoted: gomez, mimetype: 'audio/mp3', duration: -9999999, ptt: true})
+client.sendMessage(from, audio, MessageType.audio, {quoted: gomez, mimetype: 'audio/mp3', duration: -9999999})                
 break
                 
 }
